ea005542dea76aa126e3e22dff169b9fe2bc0501
Tristanite <tristanpaolosoguilon@gmail.com>
2026-03-11 10:01:53 +0800

    - Added path after installing choco. - Formatted choco installation to make it readable. - Added other used softwares.

--- legion-setup.ps1
+++ legion-setup.ps1
@@ -2,21 +2,34 @@
 if (!(Get-Command choco -ErrorAction SilentlyContinue)) {
     Write-Host "Installing Chocolatey..."
 
-    Set-ExecutionPolicy Bypass -Scope Process -Force; 
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
     iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+    # Ensure choco command works immediately
+    $env:PATH += ";$env:ALLUSERSPROFILE\chocolatey\bin"
 }
 
 Write-Host "Installing required software..."
 
-choco install git -y
-choco install python -y
-choco install vscode -y
-choco install qgis -y
-choco install 7zip -y
-choco install discord -y
-choco install dbeaver -y
-
+choco install `
+    git `
+    python `
+    vscode `
+    qgis `
+    7zip `
+    discord `
+    dbeaver `
+    firefox `
+    google-drive `
+    steam `
+    googleearthpro `
+    viber `
+    qbittorrent `
+    epicgames `
+    zoom `
+    microsoft-teams `
+    wiztree `
+    -y
 
 Write-Host "Setup complete!"
